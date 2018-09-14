3d543a3750ae62054df08d3a340cd15d24936297
Marek Szymanski <szyman.marek@gmail.com>
2018-09-14 13:23:23 +0200

    Decorator pattern

---
+++ src/decorator.ts
@@ -0,0 +1,62 @@
+//https://www.tutorialspoint.com/design_pattern/decorator_pattern.htm
+
+interface Shape {
+    type: string;
+    draw(): void;
+}
+
+class Circle implements Shape {
+    type: string = 'Circle';
+
+    draw(): void {
+        console.log('Circle::draw');
+    }
+}
+
+class Rectangle implements Shape {
+    type: string = 'Rectangle';
+
+    draw(): void {
+        console.log('Shape: Rectangle');
+    }
+}
+
+abstract class ShapeDecorator implements Shape {
+    type: string;
+    protected decoratedShape: Shape;
+
+    constructor(decoratedShape: Shape) {
+        this.decoratedShape = decoratedShape;
+    }
+
+    draw(): void {
+        this.decoratedShape.draw();
+    }
+}
+
+class RedShapeDecorator extends ShapeDecorator {
+    constructor(decoratedShape: Shape) {
+        super(decoratedShape);
+    }
+
+    draw(): void {
+        this.decoratedShape.draw();
+        this.setRedBorder(this.decoratedShape);
+    }
+
+    private setRedBorder(decoratedShape: Shape): void {
+        console.log(decoratedShape.type + ': Border Color: Red');
+    }
+}
+
+export function DecoratorPatternDemo() {
+    let circle = new Circle();
+    circle.draw();
+    let redCircle = new RedShapeDecorator(new Circle());
+    redCircle.draw();
+
+    let rect = new Rectangle();
+    rect.draw();
+    let redRect = new RedShapeDecorator(new Rectangle());
+    redRect.draw();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { ObserverPatternDemo } from './observer';
 import { FactoryPatternDemo } from './factory';
 import { PrototypePatternDemo } from './prototype';
+import { DecoratorPatternDemo } from './decorator';
 
 //ObserverPatternDemo();
 //FactoryPatternDemo();
-PrototypePatternDemo();
+//PrototypePatternDemo();
+DecoratorPatternDemo();
